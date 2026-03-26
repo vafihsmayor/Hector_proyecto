64328f5050fe4c3f44d9c7866f54773653ad3487
RonaldHZzzz <ronaldhernandez212121@gmail.com>
2026-03-26 02:16:27 -0600

    Improves module resolution for the backend
    Appends the current directory to the system path to ensure internal modules are correctly resolved during execution, preventing potential import errors in different environments.

--- Backend/application.py
+++ Backend/application.py
@@ -1,2 +1,8 @@
+import os
+import sys
+
+# Ensure the current directory is in the path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from core.wsgi import application
 app = application
